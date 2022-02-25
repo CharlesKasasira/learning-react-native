dd37cf961e790e5cdbf1aa1631cede6939241441
Charles Kasasira <charleskasasira01@gmail.com>
2022-02-25 12:39:35 +0300

    PrayerRequestScreen

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import LoginScreen from './screens/LoginScreen'
 import DashboardScreen from './screens/DashboardScreen'
 import ContactScreen from './screens/ContactScreen'
+import PrayerRequestScreen from './screens/PrayerRequestScreen'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const App = () => {
@@ -21,7 +22,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Login" >
         <Stack.Screen name='Login' component={LoginScreen} />
         <Stack.Screen name='Dashboard' component={DashboardScreen} options={{title: 'DASHBOARD'}} />
-        <Stack.Screen name='Contact' component={ContactScreen} />
+        <Stack.Screen name='Prayer' component={PrayerRequestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,60 +1,15 @@
-import { View, Text, TouchableOpacity, TextInput, Button, FlatList } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { GLOBAL_STYLES } from '../styles/style'
 
-const DashboardScreen = (props) => {
+const DashboardScreen = ({navigation}) => {
   const [ text, setText ] = useState('Charles is a Genius')
   return (
     <View style={GLOBAL_STYLES.container}>
-      {/* <View style={GLOBAL_STYLES.card}>
-        <Text>Prayer Request</Text>
-      </View>
-      <View style={GLOBAL_STYLES.card}>
-        <Text>Prayer Request</Text>
-      </View>
-      <View style={GLOBAL_STYLES.card}>
-        <Text>Prayer Request</Text> 
-      </View>
-      <View style={GLOBAL_STYLES.card}>
-        <Text>Prayer Request</Text>
-      </View> */}
-
-      <FlatList 
-        style={GLOBAL_STYLES.flat}
-        data={[
-          {name: 'One'},
-          {name: 'Two'},
-          {name: 'There'},
-          {name: 'There'},
-          {name: 'There'},
-          {name: 'There'},
-          {name: 'There'}
-        ]}
-
-        renderItem={({item}) => <View style={GLOBAL_STYLES.card}><Text>{item.name}</Text></View>}
-      />
-
-      <Text>Dashboard</Text>
-
-      <TouchableOpacity
-        onPress={() => props.navigation.navigate('Login')}>
-            <Text>Logout</Text>
+      <TouchableOpacity 
+          onPress={() => navigation.navigate('Prayer')}>
+        <Text>Prayer Requests</Text>
       </TouchableOpacity>
-      <Button title='Contact list'
-        onPress={() => props.navigation.navigate('Contact')} />
-      <Text style={GLOBAL_STYLES.geniusStaff}>{text} is a genius</Text>
-
-      <TextInput
-        placeholder='Enter the genius name'
-        onChangeText={(typed)=> {
-          setText(typed)
-        }}
-      />
-
-        {/* <Button title='Go to Login'
-            onPress={() => props.navigation.push('Login')} /> 
-        <Button title='Go Home'
-            onPress={() => props.navigation.popToTop()} /> */}
     </View>
   )
 }
