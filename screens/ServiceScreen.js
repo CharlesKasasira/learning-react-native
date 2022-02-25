1c71bd0db919f9403cd12314189040e5eaae2ff9
Charles Kasasira <charleskasasira01@gmail.com>
2022-02-25 14:31:03 +0300

    adding youtube-video

--- screens/ServiceScreen.js
+++ screens/ServiceScreen.js
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import YoutubePlayer from 'react-native-youtube-iframe'
 
 const ServiceScreen = () => {
   return (
     <View>
-      <Text>ServiceScreen</Text>
+      <YoutubePlayer 
+        height={300}
+        play={true}
+        videoId={'84WIaK3bl_s'}
+      />
     </View>
   )
 }
