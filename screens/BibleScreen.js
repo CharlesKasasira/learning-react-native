e80cba14d73a70a494aff591606ede696deb0994
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-02 13:35:32 +0300

    adding icons

--- screens/BibleScreen.js
+++ screens/BibleScreen.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import FAIcon from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/Feather'
 
 const BibleScreen = () => {
   return (
     <View>
       <Text>BibleScreen</Text>
+      <TouchableOpacity>
+        <FAIcon name='search' size={20} color='red' />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <FAIcon name='microphone' size={20} color='red' />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Icon name='pause-circle' size={20} color='red' />
+      </TouchableOpacity>
+      
     </View>
   )
 }
